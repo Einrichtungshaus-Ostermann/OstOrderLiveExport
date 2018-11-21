966b4cb10feae210577222d7a387a1d22560014e
Tim Windelschmidt <fionera@fionera.de>
2018-11-21 16:26:54 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+[![Build Status](https://travis-ci.org/Einrichtungshaus-Ostermann/OstOrderLiveExport.svg?branch=master)](https://travis-ci.org/Einrichtungshaus-Ostermann/OstOrderLiveExport)
+# Order Live Export
+Export Orders on finish to the API
---
+++ Setup/Uninstall.php
@@ -0,0 +1,85 @@
+<?php declare(strict_types=1);
+
+/**
+ * Einrichtungshaus Ostermann GmbH & Co. KG - Order Live Export
+ *
+ * @package   OstOrderLiveExport
+ *
+ * @author    Tim Windelschmidt <tim.windelschmidt@ostermann.de>
+ * @copyright 2018 Einrichtungshaus Ostermann GmbH & Co. KG
+ * @license   proprietary
+ */
+
+namespace OstOrderLiveExport\Setup;
+
+use Shopware\Bundle\AttributeBundle\Service\CrudService;
+use Shopware\Components\Model\ModelManager;
+use Shopware\Components\Plugin;
+use Shopware\Components\Plugin\Context\UninstallContext;
+
+class Uninstall
+{
+    /**
+     * Main bootstrap object.
+     *
+     * @var Plugin
+     */
+    protected $plugin;
+
+
+
+    /**
+     * ...
+     *
+     * @var UninstallContext
+     */
+    protected $context;
+
+
+
+    /**
+     * ...
+     *
+     * @var ModelManager
+     */
+    protected $modelManager;
+
+
+
+    /**
+     * ...
+     *
+     * @var CrudService
+     */
+    protected $crudService;
+
+
+
+    /**
+     * ...
+     *
+     * @param Plugin           $plugin
+     * @param UninstallContext $context
+     * @param ModelManager     $modelManager
+     * @param CrudService      $crudService
+     */
+    public function __construct(Plugin $plugin, UninstallContext $context, ModelManager $modelManager, CrudService $crudService)
+    {
+        // set params
+        $this->plugin = $plugin;
+        $this->context = $context;
+        $this->modelManager = $modelManager;
+        $this->crudService = $crudService;
+    }
+
+
+
+    /**
+     * ...
+     *
+     * @throws \Exception
+     */
+    public function uninstall()
+    {
+    }
+}
